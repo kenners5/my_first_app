786d2234c3f4835a9d4d593a3c20403e58285a2c
Ken Naishtat <kenners@gmail.com>
2013-10-05 00:50:39 -0700

    Updates settings and wsgi for hopefully the last time. Creates default sqlite database.

--- django_site/settings.py
+++ django_site/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\kenners\\Code\\notecards\\notecards\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\\Users\\kenners\\Code\\notecards\\django_site\\sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
