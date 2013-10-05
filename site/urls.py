4a70394902ced30dcf4658419a3921cf846d3bfc
Ken Naishtat <kenners@gmail.com>
2013-10-05 00:33:46 -0700

    Renames the site source folder to be 'site', so the app can be 'notecards' without too much confusion. Unleashes the PyDev auto-include resolver which did some reformatting.

--- site/urls.py
+++ site/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'notecards.views.home', name='home'),
